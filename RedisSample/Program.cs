02b6d1cc6976789022aa2a8463d10533765a7f25
Jibraan Ahmed <67121199+jibraanahmed@users.noreply.github.com>
2023-02-09 16:14:35 +0500

    redis-sample

---
+++ RedisSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using StackExchange.Redis;
+using System;
+using System.IO.Pipelines;
+
+namespace RedisSample
+{
+    internal class Program
+    {
+        static IDatabase? database = null;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PingRedis();
+            p.RedisSaveDelete();
+            Console.Read();
+        }
+
+        public async void RedisSaveDelete()
+        {
+            try
+            {
+                var res = await database.StringSetAsync("test", "test",TimeSpan.FromSeconds(10));
+                var result = await database.StringGetAsync("test");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+
+        public async void PingRedis()
+        {
+            try
+            {
+                database = RedisConnection.Initialize();
+                await database.PingAsync();
+                Console.WriteLine(database);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ RedisSample/RedisConnection.cs
@@ -0,0 +1,67 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisSample
+{
+    public class RedisConnection
+    {
+        private static ConnectionMultiplexer? REDIS;
+        public static IDatabase? DB;
+        private static EndPointCollection? EndPointsCollection;
+        public static IDatabase Initialize()
+        {
+            IDatabase? database = null;
+            try
+            {
+                EndPointsCollection = new EndPointCollection();
+                EndPointsCollection.Add("127.0.0.1", 6379);
+                database = connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return database;
+        }
+
+        private static IDatabase connect()
+        {
+            IDatabase? database = null;
+            try
+            {
+                var configurationOptions = new ConfigurationOptions();
+                configurationOptions.SyncTimeout = int.MaxValue;
+                configurationOptions.ConfigCheckSeconds = 5;
+                configurationOptions.ConnectRetry = 10;
+
+                foreach (var item in EndPointsCollection)
+                {
+                    configurationOptions.EndPoints.Add(item);
+                }
+
+                REDIS = ConnectionMultiplexer.Connect(configurationOptions);
+                DB = REDIS.GetDatabase();
+
+                if (REDIS.IsConnected)
+                {
+                    database = DB;
+                    Console.WriteLine("\t Redis Connected ");
+                }
+                else
+                {
+                    Console.WriteLine("\t Redis not Connected ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return database;
+        }
+    }
+}
